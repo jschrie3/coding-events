8e1a350940949d7bcf3a8649caf66463a489cf6f
julian <julianschriedel@gmail.com>
2023-03-21 13:41:30 -0500

    completed exercises, can't use css style because using bootstrap

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,23 +8,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Chris Bay
  */
+
+// CHAPTER 13 EXERCISES: 13.10.2. Expanding our Events Schedule
+
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    // TODO 2: In the videos, we learned how to use templates to display the elements in a static
+    //  list called events. Let’s make our events list a HashMap! This enables us to add
+    //  descriptions to our events.
 
-    @GetMapping
-    public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
-        model.addAttribute("events", events);
-        return "events/index";
-    }
+//    private static List<String> events = new ArrayList<>();
+
+    // change events list to events hashmap
+    private static HashMap<String, String> events = new HashMap<>();
+
+    // TODO 1: Comment out your previous code in the displayAllEvents method.
+//    @GetMapping
+//    public String displayAllEvents(Model model) {
+//        model.addAttribute("title", "All Events");
+//        model.addAttribute("events", events);
+//        return "events/index";
+//    }
 
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
@@ -32,18 +45,47 @@ public class EventController {
         return "events/create";
     }
 
-    @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
-        return "redirect:";
+
+    // TODO 3: Fill your events HashMap with the names and descriptions of 3 coding events around town.
+
+    // update displayAllEvents for events hashmap
+    @GetMapping
+    public String displayAllEvents(Model model){
+        HashMap<String, String> events = new HashMap<>();
+        events.put("Menteaship","A fun meetup for connecting with mentors");
+        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imaginary meetup for Javascript developers");
+        // create a model to pass data into a view
+        model.addAttribute("events", events);
+        // need to iterate over hashmap to get key/value pairs
+        for (Map.Entry<String, String> event : events.entrySet()) {
+            model.addAttribute("name", event.getKey());
+            model.addAttribute("address", event.getValue());
+        }
+        return "events/index"; // return template at events/index
     }
 
+    // comment out previous processCreateEventForm since it's acting on an arraylist object
+//    @PostMapping("create")
+//    public String processCreateEventForm(@RequestParam String eventName) {
+//        events.add(eventName);
+//        return "redirect:";
+//    }
+
+    //update processCreateEventForm for events hashmap
+    @PostMapping("create")
+    public String processCreateEventForm(@RequestParam String eventName, String eventDesc) {
+        events.put(eventName, eventDesc);
+        return "redirect:/events";
+    }
+
+    // TODO (IN EVENTS/INDEX.HTML): Using th:block and th:each, put together the events and their descriptions in a table as opposed to an unordered list.
+
+    // TODO (IN TEMPLATES/FRAGMENTS.HTML): Use fragments to store the address of the new tech hub where all of the programmers are hanging out. Use th:replace in your main template to bring in the address as a third column in your table. You may need to create a new fragments.html.
+
+    // TODO (IN STATIC/STYLES.CSS): Add some CSS to style your table to make it easier to read and center it on the page. You may need to create a new styles.css as well. Make sure to connect styles.css to the appropriate template with th:href.
+
 }
-
-
-
-
-
 
 
 
